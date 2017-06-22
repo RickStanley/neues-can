235188fe106c6af7b4f0d400ba6a1b65a629eef8
RickStanley <rick-stanley@hotmail.com>
2017-06-22 16:24:59 -0400

    Pequenos detalhes

---
+++ dev/js/zfrmnt.js
@@ -0,0 +1,10 @@
+// ----------- ATENÇÃO --------------
+// Apague após primeiro uso.
+// Utilize uma dessas funções.
+// Essa para scripts que precisam ser inicializados imediatamente.
+// (function ( window, document, $, undefined ) { CORPO })( window, document, jQuery);
+// Essa para scripts que precisam ser inicializados após DOM, no caso essa função é um short jQuery.
+// $(function () { CORPO });
+// Adicione DENTRO DO CORPO o 'use stric';
+// window.onload = function () {} // último recurso
+// Agora selecione tudo e exclua.
--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,58 @@
-'use strict';
-const     gulp          = require('gulp')
-        , jshint        = require('gulp-jshint')
-        , clean         = require('gulp-clean')
-        , concat        = require('gulp-concat')
-        , uglify        = require('gulp-uglify')
-        , header        = require('gulp-header')
-        , runSequence   = require('run-sequence')
-        , imagemin      = require('gulp-imagemin')
-        , sass          = require('gulp-sass')
-        , autoprefixer  = require('gulp-autoprefixer')
-        , cleanCSS      = require('gulp-clean-css')
-        , rename        = require('gulp-rename')
-        , browserSync   = require('browser-sync').create();
+(function () {
+    // Use strict em function form
+    'use strict';
+    const gulp = require('gulp'),
+        jshint = require('gulp-jshint'),
+        clean = require('gulp-clean'),
+        concat = require('gulp-concat'),
+        uglify = require('gulp-uglify'),
+        header = require('gulp-header'),
+        runSequence = require('run-sequence'),
+        imagemin = require('gulp-imagemin'),
+        sass = require('gulp-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        cleanCSS = require('gulp-clean-css'),
+        rename = require('gulp-rename'),
+        browserSync = require('browser-sync').create();
 
 
-gulp.task('clean', function() {
+    gulp.task('clean', function () {
         return gulp.src('res/')
             .pipe(clean());
-});
+    });
 
-// Prevents gulp break if catches erro
-function swallowError(error){
-    console.log(error.toString());
-    this.emit('end');    
-}
+    // Prevents gulp break if catches erro
+    function swallowError(error) {
+        console.log(error.toString());
+        this.emit('end');
+    }
 
-/**
- * sass task
- */
+    /**
+     * sass task
+     */
 
-gulp.task('sass', function(){
+    gulp.task('sass', function () {
         gulp.src('dev/scss/app.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS())       
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('res/css/'))
-        .pipe(browserSync.reload({stream:true}))
-});
+            .pipe(sass().on('error', sass.logError))
+            .pipe(autoprefixer())
+            .pipe(cleanCSS())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest('res/css/'))
+            .pipe(browserSync.reload({
+                stream: true
+            }))
+    });
 
 
-gulp.task('jshint', function() {
+    gulp.task('jshint', function () {
         return gulp.src('dev/js/**/*.js')
             .pipe(jshint())
             .pipe(jshint.reporter('default'));
-});
+    });
 
-gulp.task('uglify', function() {
+    gulp.task('uglify', function () {
         return gulp.src('dev/js/**/*.js')
             .pipe(uglify())
             .on('error', swallowError)
@@ -56,40 +60,45 @@ gulp.task('uglify', function() {
             .pipe(concat('main.min.js'))
             .pipe(gulp.dest('res/js/'))
             .pipe(browserSync.stream());
-});
+    });
 
-/**
- * Imagemin Task
- */
+    /**
+     * Imagemin Task
+     */
 
-gulp.task('imagemin', function() {
-    return gulp.src('dev/img/**/*.{jpg,png,gif}')
-        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('res/img/'));
-});
+    gulp.task('imagemin', function () {
+        return gulp.src('dev/img/**/*.{jpg,png,gif}')
+            .pipe(imagemin({
+                optimizationLevel: 3,
+                progressive: true,
+                interlaced: true
+            }))
+            .pipe(gulp.dest('res/img/'));
+    });
 
-//fonts
-gulp.task('fonts', function() {
+    //fonts
+    gulp.task('fonts', function () {
         return gulp.src('dev/fonts/**/*')
             .pipe(gulp.dest('res/fonts'));
-});
-
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ""
-        }
     });
-});
 
-//watch
-gulp.task('watch', function() {
+    // Static server
+    gulp.task('browser-sync', function () {
+        browserSync.init({
+            server: {
+                baseDir: ""
+            }
+        });
+    });
+
+    //watch
+    gulp.task('watch', function () {
         gulp.watch('dev/img/**/*.{png,gif,jpg}', ['imagemin'])
         gulp.watch('dev/js/**/*.js', ['uglify'])
         gulp.watch('dev/scss/**/*.scss', ['sass']);
         gulp.watch(['*']).on('change', browserSync.reload);
-})
-gulp.task('default', function(cb) {
-        return runSequence('clean',['jshint', 'uglify', 'sass', 'imagemin', 'fonts', 'watch', 'browser-sync'], cb)
-});
+    })
+    gulp.task('default', function (cb) {
+        return runSequence('clean', ['jshint', 'uglify', 'sass', 'imagemin', 'fonts', 'watch', 'browser-sync'], cb)
+    });
+}());
