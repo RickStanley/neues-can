2f0d6a0e0fe8c0e59116f8b2bdb01e80c7bebce4
RickStanley <rick-stanley@hotmail.com>
2017-09-08 14:46:53 -0400

    Adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@
             // Url validation RegExp
             const regExp = /^((https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
             if (regExp.test(process.argv[vArg + 1])) {
-                var vhost = process.argv[vArg + 1];
+                var vhost = process.argv[vArg + 1] + (process.env.PWD).substring((process.env.PWD).lastIndexOf('/')) + '/';
             } else {
                 setTimeout(function () {
                     throw new Error("Please insert a valid URL to your vhost");
@@ -141,7 +141,7 @@
     // Imagemin Task
     gulp.task('imagemin', () => {
         watcher[1] = watch(pathsSrc.images, {
-            ignoreInitial: false
+            ignoreInitial: (iArg > -1) ? false : true
         }, () => {
             gulp.src(pathsSrc.images)
                 .pipe(imagemin([
